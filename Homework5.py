d3e81ad8bf3e4ddb7ce05858f4dda24f63dd89f2
Esenka1993 <lyd_93@mail.ru>
2024-05-20 22:00:32 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,13 @@
+my_list=['apples', 'lemons', 'oranges','kiwies', 'melons','bananas']
+print(my_list)
+print(my_list[0],my_list[2])
+print(my_list [3:6])
+my_list[3]='peaches'
+print(my_list)
+my_dict={'cat':'кошка', 'dog':'собака'}
+print(my_dict)
+print(my_dict['cat'])
+my_dict['cat']='gatto'
+print(my_dict['cat'])
+my_dict['rat']='крыса'
+print(my_dict)
