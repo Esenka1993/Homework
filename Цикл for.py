4cd90a54e15a97e0aecd00d0e914cb20758cf30b
Esenka1993 <lyd_93@mail.ru>
2024-05-23 19:40:37 +0300

    Add files via upload

---
+++ Цикл for.py
@@ -0,0 +1,7 @@
+list_cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in list_cars:
+    print('Я езжу на автомобиле марки ' + i)
+cars_count = 0
+for i in range(cars_count):
+    cars_count += 10
+    print(cars_count)
