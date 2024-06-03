5843066694d0dbfb5817d39300268dd35b8ed24c
Esenka1993 <lyd_93@mail.ru>
2024-06-03 14:59:05 +0300

    Add files via upload

--- module_2_6.py
+++ module_2_6.py
@@ -1,8 +1,13 @@
-def single_root_words(root_word, *other_words):
-    same_words = []
-    for i in other_words:
-        if root_word.lower().count(i.lower()) or i.lower().count(root_word.lower()):
-            same_words.append(i)
-    return same_words
-print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
-print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+print_params()
+print_params(1, 'строка')
+print_params(1,True)
+print_params(b = 25)
+print_params(c = [1,2,3])
+values_list = ['длина', 15, True]
+values_dict = {'a': 10, 'b': 'ширина', 'c': False}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [36.6, 'temperature']
+print_params(*values_list_2, 42)
