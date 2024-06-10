75d7476740da16938e136592b2eec582b408d386
Esenka1993 <lyd_93@mail.ru>
2024-06-10 19:13:36 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as d_fm
+from true_math import divide as d_tm
+
+
+result1 = d_fm(69, 3)
+result2 = d_fm(3, 0)
+result3 = d_tm(49, 7)
+result4 = d_tm(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
