b5c17ebed4feb13c65d52149e4c505d612c61074
Esenka1993 <lyd_93@mail.ru>
2024-07-17 21:53:00 +0300

    Update module_7_3_1.py

--- module_7_3_1.py
+++ module_7_3_1.py
@@ -1,19 +1,15 @@
 class WordsFinder:
     def __init__(self, *file_names: list):
         self.file_names = file_names
-        #for name in self.file_names:
-          #file_names = f'{name}.txt'
-
+        
     def get_all_words(self):
         all_words = {}
         for file_name in self.file_names:
             with open(file_name, 'r', encoding ='utf-8') as file:
                 words = []
-                file.read().lower().strip()
-                words = file.read().replace(',', '').replace('.', '').replace('=', '').replace('!', '').replace('?',
+                words = file.read().lower().replace(',', '').replace('.', '').replace('=', '').replace('!', '').replace('?',
                                                                                                             '').replace(
                         ';', '').replace(':', '').replace(' - ', ' ').split()
-                #words.extend(line.split())
                 all_words[file_name] = words
             return all_words
 
@@ -21,7 +17,7 @@ class WordsFinder:
         word_positions = {}
         all_words = self.get_all_words()
         for file_name, words in all_words.items():
-            if word in words:
+            if word.lower() in words:
                 word_positions[file_name] = words.index(word)
         return word_positions
 
@@ -29,7 +25,8 @@ class WordsFinder:
         word_counts = {}
         all_words = self.get_all_words()
         for file_name, words in all_words.items():
-            word_counts[file_name] = words.count(word)
+            if word.lower() in words:
+                word_counts[file_name] = words.count(word)
         return word_counts
 
 finder2 = WordsFinder('test_file.txt')
