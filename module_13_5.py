79fd598b0805010139092c493abaa0483d6da968
Esenka1993 <lyd_93@mail.ru>
2024-09-19 20:18:56 +0300

    Update module_13_5.py

--- module_13_5.py
+++ module_13_5.py
@@ -52,5 +52,9 @@ async def send_calories(message, state):
     await state.finish()
 
 
+@dp.message_handler()
+async def all_messages(message):
+    await message.answer('Введите команду /start, чтобы начать общение')
+
 if __name__=='__main__':
     executor.start_polling(dp, skip_updates=True)
